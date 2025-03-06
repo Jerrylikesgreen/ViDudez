11496d9a07d5743604a77d273ed554a0e65e2c3b
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-03-05 22:26:26 -0500

    Started working on the logic behind managing Vidudz needs.

--- Scripts/Vi_Dudez/vi_dudez_ai.gd
+++ Scripts/Vi_Dudez/vi_dudez_ai.gd
@@ -1,30 +1,39 @@
 extends World_
-class_name ViDudez_State_Machine
+class_name ViDudez_Brain
 
 ##                                             >                     Resource which contains an array called Vi States
 @onready var ViDudez_States: Resource 
-
+@onready var _is_hungry: bool = false
 ##                                              >                       Holds current state - none currenrtly defined. 
 @export var active_state = "Idle"
-
+@export var combat_stats = Resource
+@export var needs = Resource
 
 
 func _ready() -> void:
+	combat_stats = ViDudez_States.get("combat_stats")
+	needs = ViDudez_States.get("needs")
 	pass 
 
 
 func _process(_delta: float) -> void:
+	
+	if needs.indices/0 >= 50:
+		_is_hungry = true
+		print("_is_hunger got triggered.")
+		print(_is_hungry)
+	
 	pass
 
 
-##                                               >                     Currently checks the active_state and returns it. 
+##                     ->->                       >                     Currently checks the active_state and returns it. 
 func _get_state() -> String:
 	var current_state = active_state
 	if current_state == null:
 		print("Error NULL")
 	return current_state
 	
-##                                               >                       Excpects to receive a String defining a state,
+##                     ->->                       >                      Excpects to receive a String defining a state,
 ##                                                                       then saves the prior state and finnally sets 
 ##                                                                       active_state with setting_state
 func _set_state(vi_states:String) -> String:
@@ -32,4 +41,9 @@ func _set_state(vi_states:String) -> String:
 	if setting_state == active_state:
 		print("State already set.")
 	return setting_state
+	@warning_ignore("unreachable_code")
 	active_state = setting_state
+	
+
+	
+	
